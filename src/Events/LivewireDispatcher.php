b8591737d0acd48d3c9fbddba4b54b49bac62f97
gwleuverink <willem@leuver.ink>
2025-10-14 14:52:48 +0200

    rewrite livewire event forwarding

---
+++ src/Events/LivewireDispatcher.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Native\Desktop\Events;
+
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Support\Facades\Event;
+use Native\Desktop\Drivers\Electron\ElectronServiceProvider;
+
+class LivewireDispatcher
+{
+    public function register(): void
+    {
+        Event::listen(function (RequestHandled $event) {
+            $this->handle($event);
+        });
+    }
+
+    /** Injects assets inside every full-page response */
+    public function handle(RequestHandled $handled)
+    {
+        $identifier = 'NativePHP Livewire Dispatcher';
+
+        if (! $handled->response->isSuccessful()) {
+            return;
+        }
+
+        $html = $handled->response->getContent();
+
+        // Skip if request doesn't return a full page
+        if (! str_contains($html, '</html>')) {
+            return;
+        }
+
+        // Skip if core was included before
+        if (str_contains($html, "<!--[{$identifier}]-->")) {
+            return;
+        }
+
+        // Get the JS dispatcher script
+        $javascript = file_get_contents(
+            ElectronServiceProvider::electronPath('electron-plugin/src/preload/livewire-dispatcher.js')
+        );
+
+        // Keep a copy of the original response
+        $originalContent = $handled->response->original;
+
+        $handled->response->setContent(
+            $this->inject($html, <<< HTML
+            <!--[{$identifier}]-->
+            <script type="module">
+            {$javascript}
+            </script>
+            HTML)
+        );
+
+        $handled->response->original = $originalContent;
+    }
+
+    /** Injects assets into given html string (taken from Livewire's injection mechanism) */
+    protected function inject(string $html, string $assets): string
+    {
+        $html = str($html);
+        $assets = PHP_EOL.$assets.PHP_EOL;
+
+        if ($html->test('/<\s*\/\s*head\s*>/i')) {
+            return $html
+                ->replaceMatches('/(<\s*\/\s*head\s*>)/i', $assets.'$1')
+                ->toString();
+        }
+
+        return $html
+            ->replaceMatches('/(<\s*\/\s*html\s*>)/i', $assets.'$1')
+            ->toString();
+    }
+}
